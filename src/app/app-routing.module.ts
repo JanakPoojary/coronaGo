146398c1ee462548798e5d156502625f1f6d9c28
JanakPoojary <janakrajpoojari@gmail.com>
2020-04-23 12:07:31 +0530

    pie-graph component routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { DataTableComponent } from './data-table/data-table.component';
 import { LineGraphComponent } from './line-graph/line-graph.component';
+import { PieGraphComponent } from './pie-graph/pie-graph.component';
 
 
 const routes: Routes = [
   { path: 'home', component: MainNavComponent, children:[
     {path:'',redirectTo:'dashboard',pathMatch:'full'},
     { path: 'dashboard', component: DataTableComponent},
-    {path: '', component: LineGraphComponent, outlet:'graph'}
+    {path: '', component: LineGraphComponent, outlet:'line-graph'},
+    {path: '', component: PieGraphComponent, outlet:'pie-graph'}
   ]},
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
